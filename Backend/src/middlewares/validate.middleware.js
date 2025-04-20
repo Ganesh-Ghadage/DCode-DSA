5c838fa9d57413eb280c61568f4253320f6156b3
Ganesh Ghadage <ganeshvg22@gmail.com>
2025-04-20 16:24:03 +0530

    feat: add validation middleware to handle request data validation

---
+++ Backend/src/middlewares/validate.middleware.js
@@ -0,0 +1,21 @@
+import { validationResult } from 'express-validator'
+import { ApiError } from '../utils/ApiError.js'
+
+const validator = (req, res, next) => {
+  const errors = validationResult(req)
+
+  if(!errors) {
+    next()
+  }
+
+  const extractedError = []
+  errors.array().map(err => {
+    extractedError.push({
+      [err.path]: err.msg
+    })
+  }) 
+
+  throw new ApiError(422, "Data is not valid", extractedError)
+}
+
+export default validator
