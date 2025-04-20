008c32880780f4820968197ec9f328838be84255
Ganesh Ghadage <ganeshvg22@gmail.com>
2025-04-20 16:23:48 +0530

    feat: implement error handling middleware

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import errorHandler from './middlewares/errors.middleware'
 
 dotenv.config()
 
@@ -8,6 +9,11 @@ const app = express()
 const PORT = process.env.PORT || 8000
 
 app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+
+
+app.use(errorHandler)
 
 app.listen(PORT, () => {
   console.log(`App is listning to port: ${PORT}`)
