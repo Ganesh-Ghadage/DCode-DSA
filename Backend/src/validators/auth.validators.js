45731861b593157aacf5434ecf2d2f28c3e6c300
Ganesh Ghadage <ganeshvg22@gmail.com>
2025-04-20 16:32:54 +0530

    feat: add user registration validation using express-validator

---
+++ Backend/src/validators/auth.validators.js
@@ -0,0 +1,19 @@
+import { body } from 'express-validator'
+
+export const userRegistrationValidatior = () => {
+  return [
+    body("email")
+      .trim()
+      .notEmpty().withMessage("Email is required")
+      .isEmail().withMessage("Invalid Email format"),
+    body("name")
+      .trim()
+      .notEmpty().withMessage("Name is required"),
+    body("password")
+      .trim()
+      .notEmpty().withMessage("Password is required")
+      .isLength({min: 8}).withMessage("Username should be more than 8 char")
+      .isLength({max: 20}).withMessage("Username cannot exceed more than 20 char")
+      .isStrongPassword({minUppercase: 1, minNumbers: 2,  minSymbols: 1}).withMessage("Password should contain at least 1 Uppcase letter, 1 Symbol and at least 2 number"),
+  ]
+}
