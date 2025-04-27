962517d37a45040723978ce5c3f47c518974462c
Ganesh Ghadage <ganeshvg22@gmail.com>
2025-04-27 18:39:42 +0530

    feat: add axios dependency and implement Judge0 utility functions

---
+++ Backend/src/utils/jude0.utils.js
@@ -0,0 +1,44 @@
+import axios from 'axios'
+
+export const getJudge0LangunageId = (language) => {
+  const languageMap = {
+    "PYTHON": 71,
+    "JAVA": 62,
+    "JAVASCRIPT": 63
+  }
+
+  return languageMap[language.toUpperCase()]
+}
+
+export const submitBatch = async (submissions) => {
+  const { data } = axios.post(`${process.env.JUDGE0_API_URL}/submissions/batch?base64_encoded=false`, { submissions })
+
+  return data
+}
+
+const sleep = (ms) => (
+  new Promise(resolve => (
+    setTimeout(resolve, ms)
+  ))
+)
+
+export const pollbatchResults = async (tokens) => {
+  while(true) {
+    const { data } = axios.get(`${process.env.JUDGE0_API_URL}/submissions/batch`, {
+      params: {
+        tokens: tokens.join(","),
+        base64_encoded: false
+      }
+    })
+  
+    const results = data.submissions
+
+    const allDone = results.every(r => r.status.id >= 3) 
+
+    if(allDone) {
+      return results
+    }
+
+    await sleep(1000)
+  }
+}
