46150184920e1dd005ab1744e716cd9f3603fa5e
Ganesh Ghadage <ganeshvg22@gmail.com>
2025-05-04 18:38:35 +0530

    fix: correct import and usage of WaitList component in App

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,11 +1,29 @@
 import './App.css'
+import Dashboard from './components/sections/Dashboard'
+import FAQ from './components/sections/FAQ'
+import Features from './components/sections/Features'
+import Footer from './components/sections/Footer'
+import Header from './components/sections/Header'
 import Hero from './components/sections/Hero'
+import WaitLits from './components/sections/WaitList'
 
 function App() {
 
   return (
     <div className='w-full'>
+      <Header />
+
       <Hero />
+
+      <WaitLits />
+
+      <Features />
+
+      <Dashboard />
+
+      <FAQ />
+
+      <Footer />
     </div>
   )
 }
