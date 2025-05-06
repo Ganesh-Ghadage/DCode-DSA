293430d93bed18cfb2f0c07c4cdea38f97993d68
Ganesh Ghadage <ganeshvg22@gmail.com>
2025-05-06 12:23:43 +0530

    feat: implement getAllSubmission controller to fetch user submissions

--- Backend/src/controllers/submissions.controllers.js
+++ Backend/src/controllers/submissions.controllers.js
@@ -1,6 +1,27 @@
+import { db } from "../libs/db.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponce } from "../utils/ApiResponce.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
-export const getAllSubmission = asyncHandler(async (req, res) => {});
+export const getAllSubmission = asyncHandler(async (req, res) => {
+	const userId = req.user.id;
+
+	const submissions = await db.Submission.findMany({
+		where: {
+			userId,
+		},
+	});
+
+	if (!submissions) {
+		throw new ApiError(400, "Submissions not found the user");
+	}
+
+	res
+		.status(200)
+		.json(
+			new ApiResponce(200, submissions, "All submissions fetched successfully")
+		);
+});
 
 export const getSubmissionForProblem = asyncHandler(async (req, res) => {});
 
