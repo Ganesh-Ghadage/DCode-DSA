b23ca83a7dd26669e2dc26b2471450ac7946c557
Ganesh Ghadage <ganeshvg22@gmail.com>
2025-05-09 17:12:24 +0530

    refactor: standardize code formatting and improve readability in middleware files

--- Backend/src/middlewares/errors.middleware.js
+++ Backend/src/middlewares/errors.middleware.js
@@ -1,23 +1,31 @@
-import { Prisma } from "../generated/prisma/index.js"
-import { ApiError } from "../utils/ApiError.js"
+import { Prisma } from "../generated/prisma/index.js";
+import { ApiError } from "../utils/ApiError.js";
 
 const errorHandler = (err, req, res, next) => {
-  let error = err
+	let error = err;
 
-  if(!(error instanceof ApiError)) {
-    const statusCode = error.statusCode || error instanceof Prisma.PrismaClientKnownRequestError ? 400 : 500
-    const errorMessage = error?.message || "Something Went Wrong"
+	if (!(error instanceof ApiError)) {
+		const statusCode =
+			error.statusCode || error instanceof Prisma.PrismaClientKnownRequestError
+				? 400
+				: 500;
+		const errorMessage = error?.message || "Something Went Wrong";
 
-    error = new ApiError(statusCode, errorMessage, error?.error || [], error?.stack)
-  }
+		error = new ApiError(
+			statusCode,
+			errorMessage,
+			error?.error || [],
+			error?.stack
+		);
+	}
 
-  const response = {
-    ...error,
-    message: error.message,
-    ...(process.env.NODE_ENV === "development" ? {stack: error?.stack} : {})
-  }
+	const response = {
+		...error,
+		message: error.message,
+		...(process.env.NODE_ENV === "development" ? { stack: error?.stack } : {}),
+	};
 
-  return res.status(error.statusCode).json(response)
-}
+	return res.status(error.statusCode).json(response);
+};
 
-export default errorHandler
+export default errorHandler;
