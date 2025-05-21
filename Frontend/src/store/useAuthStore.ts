82e0b41f5699492b33ea1836719255d9dd7225c5
Ganesh Ghadage <ganeshvg22@gmail.com>
2025-05-21 13:13:30 +0530

    feat: add functionallity for signup, login, and logout actions in useAuthStore

--- Frontend/src/store/useAuthStore.ts
+++ Frontend/src/store/useAuthStore.ts
@@ -1,5 +1,6 @@
 import { create } from "zustand";
 import { axiosInstance } from "../lib/axios";
+import toast from "react-hot-toast";
 
 interface signupData {
   name: string,
@@ -33,22 +34,55 @@ export const useAuthStore = create<AuthState>()((set) => ({
   errorMessage: null,
 
   checkAuth: async () => {
-    const res = await axiosInstance.get("/profile")
-    
-    console.log(res)
+    set({ isCheckingAuth: true })
+    try {
+      const res = await axiosInstance.get("/auth/profile")
+      set({ authUser: res.data.data })
+    } catch (error) {
+      set({ errorMessage: (error instanceof Error && error.message) ? error.message : "Something went wrong" })
+      set({ authUser: null })
+    } finally {
+      set({ isCheckingAuth: false })
+    }
   },
 
   signup: async (data) => {
-
+    set({ isSigningUp: true })
+    try {
+      const res = await axiosInstance.post("/auth/register", data)
+      set({ authUser: res.data.data })
+      toast.success(res.data?.message || "Signup successfull")
+    } catch (error) {
+      set({ errorMessage: (error instanceof Error && error.message) ? error.message : "Something went wrong" })
+      console.log(error)
+    } finally {
+      set({ isSigningUp: false })
+    }
   },
 
   login: async (data) => {
-
+    set({ isLoggingIn: true })
+    try {
+      const res = await axiosInstance.post("/auth/login", data)
+      set({ authUser: res.data.data })
+      toast.success(res.data?.message || "login successfull")
+    } catch (error) {
+      set({ errorMessage: (error instanceof Error && error.message) ? error.message : "Something went wrong" })
+      console.log(error)
+    } finally {
+      set({ isLoggingIn: false })
+    }
   },
 
   logout: async () => {
-
+    try {
+      await axiosInstance.post("/auth/logout")
+      set({ authUser: null })
+      toast.success("logout successfull")
+    } catch (error) {
+      set({ errorMessage: (error instanceof Error && error.message) ? error.message : "Something went wrong" })
+      console.log(error)
+    }
   }
 
 }))
-
