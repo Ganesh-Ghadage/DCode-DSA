3c9261e7f909ac78640b63317abb906980cc33c8
Ganesh Ghadage <ganeshvg22@gmail.com>
2025-05-21 13:31:57 +0530

    feat: add loading state and authentication check to App component

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,34 +1,49 @@
-import { Route, Routes, Navigate } from "react-router-dom"
-import HomePage from "./pages/HomePage"
-import LoginPage from "./pages/LoginPage"
-import SignupPage from "./pages/SignupPage"
-import { Toaster } from "react-hot-toast"
+import { Route, Routes, Navigate } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import { Loader } from "lucide-react";
+
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+import { useAuthStore } from "./store/useAuthStore";
+import { useEffect } from "react";
 
 function App() {
-  let authUser = null
+	const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
 
-  return (
-    <div className="flex flex-col justify-start items-center">
-      <Toaster />
-      <Routes>
+	useEffect(() => {
+		checkAuth();
+	}, []);
 
-        <Route 
-          path="/"
-          element={authUser ? <HomePage /> : <Navigate to={"/login"} />}
-        />
+	if (!authUser && isCheckingAuth) {
+		return (
+			<div className="flex items-center justify-center h-screen">
+				<Loader className="size-10 animate-spin" />
+			</div>
+		);
+	}
 
-        <Route 
-          path="/login"
-          element={!authUser ? <LoginPage /> : <Navigate to={"/"} />}
-        />
+	return (
+		<div className="flex flex-col justify-start items-center">
+			<Toaster />
+			<Routes>
+				<Route
+					path="/"
+					element={authUser ? <HomePage /> : <Navigate to={"/login"} />}
+				/>
 
-        <Route 
-          path="/signup"
-          element={!authUser ? <SignupPage /> : <Navigate to={"/"} />}
-        />
-      </Routes>
-    </div>
-  )
+				<Route
+					path="/login"
+					element={!authUser ? <LoginPage /> : <Navigate to={"/"} />}
+				/>
+
+				<Route
+					path="/signup"
+					element={!authUser ? <SignupPage /> : <Navigate to={"/"} />}
+				/>
+			</Routes>
+		</div>
+	);
 }
 
-export default App
+export default App;
