fb311488be73e21a4ba6036a666cea3ac8fe578c
Ganesh Ghadage <ganeshvg22@gmail.com>
2025-05-21 12:18:35 +0530

    feat: add CORS support to the backend

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 import errorHandler from "./middlewares/errors.middleware.js";
 
@@ -20,6 +21,12 @@ const app = express();
 
 const PORT = process.env.PORT || 8000;
 
+app.use(
+	cors({
+		origin: "http://localhost:5173",
+		credentials: true,
+	})
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
