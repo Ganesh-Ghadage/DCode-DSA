159336146da3f99077a346a119f1f1f320df30d6
Ganesh Ghadage <ganeshvg22@gmail.com>
2025-05-24 14:42:58 +0530

    feat: implement useSubmissionStore for managing submission state and fetching data

---
+++ Frontend/src/store/useSubmissionStore.ts
@@ -0,0 +1,86 @@
+import { create } from 'zustand'
+import toast from 'react-hot-toast'
+import { AxiosError } from 'axios'
+
+import { axiosInstance } from '../lib/axios'
+import type { Submission } from '../types'
+
+interface SubmissionState {
+  allSubmissions: Submission[]
+  problemSubmissions: Submission[]
+  submissionCount: number | null
+  isALLSubmissionLodading: boolean
+  isProblemSubmissionLodading: boolean
+  isCountLoading: boolean
+  errorMessage: string | null
+
+  getAllSubmissions: () => void
+  getSubmissionForProblem: (problemId: string) => void
+  getSubmissionCountForProblem: (problemId: string) => void
+}
+
+export const useSubmissionStore = create<SubmissionState>()((set) => ({
+  allSubmissions: [],
+  problemSubmissions: [],
+  submissionCount: null,
+  isALLSubmissionLodading: false,
+  isProblemSubmissionLodading: false,
+  isCountLoading: false,
+  errorMessage: null,
+
+  getAllSubmissions: async () => {
+    try {
+      set({ isALLSubmissionLodading: true })
+      const res = await axiosInstance.get("/submissions/get-all-submissions")
+      set({ allSubmissions: res.data.data })
+      toast.success(res.data.message)
+    } catch (error) {
+      set({ errorMessage: (error instanceof Error && error.message) ? error.message : "Something went wrong" })
+      set({ allSubmissions: [] })
+      toast.error(
+        error instanceof AxiosError && error?.response?.data.message
+          ? error.response.data.message
+          : "Something went wrong"
+      );
+    } finally {
+      set({ isALLSubmissionLodading: false })
+    }
+  },
+
+  getSubmissionForProblem: async (problemId) => {
+    try {
+      set({ isProblemSubmissionLodading: true })
+      const res = await axiosInstance.get(`/submissions/get-submission/${problemId}`)
+      set({ problemSubmissions: res.data.data })
+    } catch (error) {
+      set({ errorMessage: (error instanceof Error && error.message) ? error.message : "Something went wrong" })
+      set({ problemSubmissions: [] })
+      toast.error(
+        error instanceof AxiosError && error?.response?.data.message
+          ? error.response.data.message
+          : "Something went wrong"
+      );
+    } finally {
+      set({ isProblemSubmissionLodading: false })
+    }
+  },
+
+  getSubmissionCountForProblem: async (problemId) => {
+    try {
+      set({ isCountLoading: true })
+      const res = await axiosInstance.get(`/submissions/get-submissions-count/${problemId}`)
+      set({ submissionCount: res.data.data.count })
+    } catch (error) {
+      set({ errorMessage: (error instanceof Error && error.message) ? error.message : "Something went wrong" })
+      set({ submissionCount: null })
+      // toast.error(
+      //   error instanceof AxiosError && error?.response?.data.message
+      //     ? error.response.data.message
+      //     : "Something went wrong"
+      // );
+    } finally {
+      set({ isCountLoading: false })
+    }
+  }
+
+}))
