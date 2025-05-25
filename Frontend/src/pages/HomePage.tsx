08f00ed5bff8f27a3be3bf32cc85837a8279645c
Ganesh Ghadage <ganeshvg22@gmail.com>
2025-05-25 14:23:04 +0530

    refactor: remove problem fetching logic and loading state from HomePage component

--- Frontend/src/pages/HomePage.tsx
+++ Frontend/src/pages/HomePage.tsx
@@ -1,23 +1,5 @@
-import { useEffect } from "react";
-import { Loader } from "lucide-react";
-
-import { useProblemStore } from "../store/useProblemStore";
-import ProblemsTable from "../components/ProblemsTable";
 
 const HomePage = () => {
-	const { getAllProblems, problems, isProblemsLoading } = useProblemStore();
-
-	useEffect(() => {
-		getAllProblems();
-	}, [getAllProblems]);
-
-	if (isProblemsLoading) {
-		return (
-			<div className="flex items-center justify-center h-screen w-4xl">
-				<Loader className="size-10 animate-spin" />
-			</div>
-		);
-	}
 
 	return (
 		<section className="min-h-screen flex flex-col items-center mt-14 px-4">
@@ -31,13 +13,7 @@ const HomePage = () => {
 				problems
 			</p>
 
-			{problems.length > 0 ? (
-				<ProblemsTable problems={problems} />
-			) : (
-				<p className="mt-10 text-center text-lg font-semibold text-gray-500 dark:text-gray-400 z-10 border border-primary px-4 py-2 rounded-md border-dashed">
-					No Problems found!
-				</p>
-			)}
+			
 		</section>
 	);
 };
