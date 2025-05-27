68ce1b1c9220154cf46eee4730f72e4d7fe77555
Ganesh Ghadage <ganeshvg22@gmail.com>
2025-05-27 12:45:27 +0530

    feat: implement sidebar and topbar menus with responsive drawer functionality

--- Frontend/src/components/Navbar.tsx
+++ Frontend/src/components/Navbar.tsx
@@ -1,127 +1,121 @@
 import { User, Code, LogOut } from "lucide-react";
-import { Link, useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 import { useAuthStore } from "../store/useAuthStore";
 import LogoutButton from "./LogoutButton";
 import Logo from "./Logo";
-import { cn } from "../lib/utils";
+import { useMobileView } from "@/customHooks/useMobileView";
+import SideBarMenu from "./ui/sidebar-menu";
+import { TopBar } from "./ui/topbar-menu";
+import ThemeToggle from "./ThemeToggleButton";
 
 function Navbar() {
 	const { authUser } = useAuthStore();
-	const path = useLocation().pathname;
+	const isMobile = useMobileView();
+
+	const navItems = [
+		{ name: "Home", url: "" },
+		{ name: "Problems", url: "problem" },
+		{ name: "Sheets", url: "sheets" },
+	];
 
 	return (
-		<nav className="sticky top-0 z-50 w-full py-5">
-			<div className="flex w-full justify-between mx-auto max-w-4xl bg-black/15 shadow-lg shadow-neutral-600/5 backdrop-blur-lg border border-gray-200/10 p-4 rounded-2xl">
-				{/* Logo Section */}
-				<Link
-					to="/"
-					className="flex items-center gap-3 cursor-pointer"
-				>
-					<Logo />
-					<span className="text-lg md:text-2xl font-bold tracking-tight text-white hidden md:block">
-						DCode DSA
-					</span>
-				</Link>
-
-				<div className="flex items-center gap-10 mx-auto">
-					<Link
-						to={"/"}
-						className={cn(
-							"text-lg md:text-xl font-bold hover:text-primary",
-							path === "/" ? "bg-primary/20 py-2 px-4 rounded-xl" : ""
-						)}
-					>
-						Home
-					</Link>
-					<Link
-						to={"/problem"}
-						className={cn(
-							"text-lg md:text-xl font-bold hover:text-primary",
-							path === "/problem" ? "bg-primary/20 py-2 px-4 rounded-xl" : ""
-						)}
-					>
-						Problems
-					</Link>
-					<Link
-						to={"/sheets"}
-						className={cn(
-							"text-lg md:text-xl font-bold hover:text-primary",
-							path === "/sheets" ? "bg-primary/20 py-2 px-4 rounded-xl" : ""
-						)}
-					>
-						Sheets
-					</Link>
-				</div>
-
-				{/* User Profile and Dropdown */}
-				{authUser ? (
-					<div className="flex items-center gap-8">
-						<div className="dropdown dropdown-end">
-							<label
-								tabIndex={0}
-								className="btn btn-ghost btn-circle avatar flex flex-row "
-							>
-								<div className="w-10 rounded-full ">
-									<img
-										src={
-											authUser?.image ||
-											"https://avatar.iran.liara.run/public/boy"
-										}
-										alt="User Avatar"
-										className="object-cover"
-									/>
-								</div>
-							</label>
-							<ul
-								tabIndex={0}
-								className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 space-y-3"
-							>
-								{/* Admin Option */}
-
-								{/* Common Options */}
-								<li>
-									<p className="text-base font-semibold">{authUser?.name}</p>
-									<hr className="border-gray-200/10" />
-								</li>
-								<li>
-									<Link
-										to="/profile"
-										className="hover:bg-primary hover:text-white text-base font-semibold"
-									>
-										<User className="w-4 h-4 mr-2" />
-										My Profile
-									</Link>
-								</li>
-								{authUser?.role === "ADMIN" && (
-									<li>
-										<Link
-											to="/add-problem"
-											className="hover:bg-primary hover:text-white text-base font-semibold"
-										>
-											<Code className="w-4 h-4 mr-1" />
-											Add Problem
-										</Link>
-									</li>
-								)}
-								<li>
-									<LogoutButton className="hover:bg-primary hover:text-white">
-										<LogOut className="w-4 h-4 mr-2" />
-										Logout
-									</LogoutButton>
-								</li>
-							</ul>
-						</div>
+		<nav className="sticky top-0 left-1/2 z-50 w-full md:px-20 bg-base-200">
+			<div className="relative flex p-4 justify-between items-center">
+				{isMobile ? (
+					<div className="w-full flex justify-between items-center">
+						<Link
+							to="/"
+							className="flex items-center gap-3 cursor-pointer"
+						>
+							<Logo />
+						</Link>
+						<SideBarMenu items={navItems} />
 					</div>
 				) : (
-					<button className="btn btn-primary">
+					<div className="w-full flex justify-between items-center">
 						<Link
-							to={"/login"}
-							className=""
+							to="/"
+							className="flex items-center gap-3 cursor-pointer"
 						>
-							LogIn
+							<Logo />
+							<span className="absolute left-18 text-lg md:text-2xl font-bold tracking-tight hidden lg:block">
+								DCode DSA
+							</span>
 						</Link>
-					</button>
+						<TopBar items={navItems} />
+
+						<div className="flex gap-2 items-center">
+							<ThemeToggle />
+							{authUser ? (
+								<div className="flex items-center gap-8">
+									<div className="dropdown dropdown-end">
+										<label
+											tabIndex={0}
+											className="btn btn-ghost btn-circle avatar flex flex-row "
+										>
+											<div className="w-10 rounded-full ">
+												<img
+													src={
+														authUser?.image ||
+														"https://avatar.iran.liara.run/public/boy"
+													}
+													alt="User Avatar"
+													className="object-cover"
+												/>
+											</div>
+										</label>
+										<ul
+											tabIndex={0}
+											className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 space-y-3"
+										>
+											<li>
+												<p className="text-base font-semibold">
+													{authUser?.name}
+												</p>
+												<hr className="border-gray-200/10" />
+											</li>
+											<li>
+												<Link
+													to="/profile"
+													className="hover:bg-primary hover:text-white text-base font-semibold"
+												>
+													<User className="w-4 h-4 mr-2" />
+													My Profile
+												</Link>
+											</li>
+											{authUser?.role === "ADMIN" && (
+												<li>
+													<Link
+														to="/add-problem"
+														className="hover:bg-primary hover:text-white text-base font-semibold"
+													>
+														<Code className="w-4 h-4 mr-1" />
+														Add Problem
+													</Link>
+												</li>
+											)}
+											<li>
+												<LogoutButton className="hover:bg-primary hover:text-white">
+													<LogOut className="w-4 h-4 mr-2" />
+													Logout
+												</LogoutButton>
+											</li>
+										</ul>
+									</div>
+								</div>
+							) : (
+								<button className="btn btn-primary">
+									<Link
+										to={"/login"}
+										className=""
+									>
+										LogIn
+									</Link>
+								</button>
+							)}
+						</div>
+					</div>
 				)}
 			</div>
 		</nav>
---
+++ Frontend/src/components/ui/sidebar-menu.tsx
@@ -0,0 +1,143 @@
+import { useState } from "react";
+import { Drawer, DrawerContent } from "@/components/ui/drawer";
+import { X, Menu, LogOut, Code, User } from "lucide-react";
+import { cn } from "@/lib/utils";
+import { TopBar } from "./topbar-menu";
+import ThemeToggle from "../ThemeToggleButton";
+import { Link } from "react-router-dom";
+import LogoutButton from "../LogoutButton";
+import { useAuthStore } from "@/store/useAuthStore";
+
+interface NavItem {
+	name: string;
+	url: string;
+}
+
+interface NavBarProps {
+	items: NavItem[];
+	className?: string;
+}
+
+function SideBarMenu({ items, className }: NavBarProps) {
+	const [open, setOpen] = useState(false);
+	const { authUser } = useAuthStore();
+
+	return (
+		<div className={cn("", className)}>
+			<div className="flex flex-col gap-4">
+				<div className="flex gap-2">
+					<button
+						role="button"
+						className="p-2 bg-border rounded-full"
+						onClick={() => {
+							setOpen(true);
+						}}
+					>
+						<Menu className="text-primary-content hover:scale-105" />
+					</button>
+				</div>
+
+				<Drawer
+					open={open}
+					onOpenChange={setOpen}
+				>
+					<DrawerContent side={"right"}>
+						<div className="w-full flex flex-col items-start gap-5 bg-base-300 h-screen p-4">
+							<div className="flex gap-2 w-full items-center justify-between">
+								<ThemeToggle />
+                <button
+									role="button"
+									className="p-2 bg-border rounded-full"
+									onClick={() => {
+										setOpen(false);
+									}}
+								>
+									<X className="text-primary-content hover:scale-105" />
+								</button>
+								
+							</div>
+
+							<div className="self-end">
+								{authUser ? (
+									<div className="flex items-center gap-8">
+										<div className="dropdown dropdown-end">
+											<label
+												tabIndex={0}
+												className="btn btn-ghost btn-circle avatar flex flex-row "
+											>
+												<div className="w-10 rounded-full ">
+													<img
+														src={
+															authUser?.image ||
+															"https://avatar.iran.liara.run/public/boy"
+														}
+														alt="User Avatar"
+														className="object-cover"
+													/>
+												</div>
+											</label>
+											<ul
+												tabIndex={0}
+												className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 space-y-3"
+											>
+												<li>
+													<p className="text-base font-semibold">
+														{authUser?.name}
+													</p>
+													<hr className="border-gray-200/10" />
+												</li>
+												<li>
+													<Link
+														to="/profile"
+														className="hover:bg-primary hover:text-white text-base font-semibold"
+													>
+														<User className="w-4 h-4 mr-2" />
+														My Profile
+													</Link>
+												</li>
+												{authUser?.role === "ADMIN" && (
+													<li>
+														<Link
+															to="/add-problem"
+															className="hover:bg-primary hover:text-white text-base font-semibold"
+														>
+															<Code className="w-4 h-4 mr-1" />
+															Add Problem
+														</Link>
+													</li>
+												)}
+												<li>
+													<LogoutButton className="hover:bg-primary hover:text-white">
+														<LogOut className="w-4 h-4 mr-2" />
+														Logout
+													</LogoutButton>
+												</li>
+											</ul>
+										</div>
+									</div>
+								) : (
+									<button className="btn btn-primary">
+										<Link
+											to={"/login"}
+											className=""
+										>
+											LogIn
+										</Link>
+									</button>
+								)}
+							</div>
+
+							<TopBar
+								items={items}
+								open={open}
+								setOpen={setOpen}
+							/>
+						</div>
+					</DrawerContent>
+				</Drawer>
+			</div>
+		</div>
+	);
+}
+
+export default SideBarMenu;
