89454c77b20f9421dfbcb88828dd20b9e3505260
Ganesh Ghadage <ganeshvg22@gmail.com>
2025-06-07 17:31:12 +0530

    feat: Enhance ErrorComponent layout with improved styling and error message display

--- Frontend/src/components/ErrorComponent.tsx
+++ Frontend/src/components/ErrorComponent.tsx
@@ -8,8 +8,9 @@ interface props {
 
 const ErrorComponent = ({ errorMessage = "Something Went Wrong" }: props) => {
 	return (
-		<div className=" text-center mt-10 -mb-20">
-			<h1 className="text-2xl text-error-content">{errorMessage}</h1>
+		<div className="flex flex-col justify-center items-center text-center mt-10 -mb-20">
+			<h1 className="text-error text-2xl font-black">Error</h1>
+			<h1 className="text-xl text-error-content">{errorMessage}</h1>
 			<div className="w-96">
 				<Lottie
 					animationData={errorAmination}
