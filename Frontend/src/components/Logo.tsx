52363b682ed35f158974bc39692e51e3847493c6
Ganesh Ghadage <ganeshvg22@gmail.com>
2025-07-04 14:40:54 +0530

    fix: Add className prop to Logo component for better customization

--- Frontend/src/components/Logo.tsx
+++ Frontend/src/components/Logo.tsx
@@ -1,14 +1,24 @@
+import { cn } from "@/lib/utils";
 
-function Logo() {
-  return (
-    <div className='w-10 h-10 rounded-full flex items-center justify-center hover:-rotate-12 hover:scale-105 transition-transform'>
-      <img 
-        className='rounded-full'
-        src="/dcodedsa_logo.jpg" 
-        alt="Company Logo" 
-      />
-    </div>
-  )
+interface LogoProps {
+	className?: String;
 }
 
-export default Logo
+function Logo({className}: LogoProps) {
+	return (
+		<div
+			className={cn(
+				"w-10 h-10 rounded-full flex items-center justify-center hover:-rotate-12 hover:scale-105 transition-transform",
+				className
+			)}
+		>
+			<img
+				className="rounded-full"
+				src="/dcodedsa_logo.jpg"
+				alt="Company Logo"
+			/>
+		</div>
+	);
+}
+
+export default Logo;
